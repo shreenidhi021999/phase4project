b409453885678347cb7b5a6129423215274bcdf4
11035905 <11035905@H5CG1220K29>
2021-10-25 12:18:08 +0530

    initial commit

---
+++ Phase4Assesment/Models/PizzaModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phase4Assesment.Models
+{
+    public class PizzaModel
+    {
+        public int Pid { get; set; }
+        public string PizzaName { get; set; }
+        public string Crust { get; set; }
+        public string Desc { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+    }
+}
